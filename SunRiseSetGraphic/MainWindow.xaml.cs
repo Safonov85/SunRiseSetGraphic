e37a26bd63ee2cb85c3b0ba931da5d91cd35e0a5
safonov <safonov1985@protonmail.com>
2021-11-30 21:47:27 +0100

    First commit again

---
+++ SunRiseSetGraphic/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SunRiseSetGraphic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CreateALine()
+        {
+            // Create a Line  
+            Line redLine = new Line();
+            redLine.X1 = 50;
+            redLine.Y1 = 50;
+            redLine.X2 = 200;
+            redLine.Y2 = 200;
+
+            // Create a red Brush  
+            SolidColorBrush redBrush = new SolidColorBrush();
+            redBrush.Color = Colors.Red;
+
+            // Set Line's width and color  
+            redLine.StrokeThickness = 4;
+            redLine.Stroke = redBrush;
+
+            // Add line to the Grid.  
+            // MainGraphImage.Source = (BitmapImage) redLine;
+        }
+
+        
+    }
+
+    
+}
