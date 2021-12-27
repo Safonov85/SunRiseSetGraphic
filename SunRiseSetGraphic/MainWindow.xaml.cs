07d42243b0639323197025f91973731dadf89fc0
safonov <safonov1985@protonmail.com>
2021-12-27 20:51:54 +0100

    Change height highlight that of Image canvas

--- SunRiseSetGraphic/MainWindow.xaml.cs
+++ SunRiseSetGraphic/MainWindow.xaml.cs
@@ -113,11 +113,11 @@ namespace SunRiseSetGraphic
                 dc.DrawEllipse(new SolidColorBrush(Color.FromArgb((byte)transparentChange, 50,50,50)), null, pointCircle, point.X * 0.5, point.X * 0.5);
 
                 //
-                dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(255, 220, 220, 40)), null, new Rect(0, 0, 20, 200));
+                dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(255, 220, 220, 40)), null, new Rect(0, 0, 20, MainGraphImage.Height));
 
-                dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(255, 220, 220, 40)), null, new Rect(30, 0, 20, 200));
+                dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(255, 220, 220, 40)), null, new Rect(30, 0, 20, MainGraphImage.Height));
 
-                dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(255, 220, 220, 40)), null, new Rect(60, 0, 20, 200));
+                dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(255, 220, 220, 40)), null, new Rect(60, 0, 20, MainGraphImage.Height));
 
                 FormattedText formattedText = new FormattedText(
         "Safonov",
